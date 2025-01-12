526e88b536b16152aa9f672d92285140ebc53475
rodgersxy <rodgersny99@gmail.com>
2025-01-12 18:54:50 +0300

    add: 1. Importing Google fonts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,8 +1,7 @@
 const Home = () => {
   return (
-    <div className="container mx-auto mt-20 text-center">
-      <h1 className="text-4xl font-bold">Welcome to Splash App!</h1>
-      <p className="mt-4 text-gray-600">This is the home page content.</p>
+    <div>
+      <h1>Welcome to Splash App</h1>
     </div>
   );
 };
